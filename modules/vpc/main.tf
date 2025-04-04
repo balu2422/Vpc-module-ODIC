d6f7ba598ce2f734c47aa4c6b45b7acf3bb4c3ae
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-04 12:35:56 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.route_table_name
+  }
+}
+
+resource "aws_route" "main" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
